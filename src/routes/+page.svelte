15e3ffb507f645a1f73eb3323bf3bdc4357b52d7
Anthony Park <kunhpark15@gmail.com>
2024-11-29 11:51:15 -0800

    add SCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<h1>You are invited to Anthony & Emily's Wedding!</h1>
+
+<style lang="scss">
+  h1 {
+    color: red;
+  }
+</style>
