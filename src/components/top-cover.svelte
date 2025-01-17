55aa749134c2ec9403ecbdaf129feb3a66bd3d47
Anthony Park <kunhpark15@gmail.com>
2025-01-16 22:38:52 -0800

    add confetti

--- src/components/top-cover.svelte
+++ src/components/top-cover.svelte
@@ -2,6 +2,7 @@
 	import { _ } from 'svelte-i18n';
 	import { localeStore } from '../i18n.svelte';
 	import { browser } from '$app/environment';
+	import { Confetti } from 'svelte-confetti';
 
 	const maxSectionHeight = 900;
 	let sectionHeight = $state(maxSectionHeight);
@@ -16,9 +17,26 @@
 	}
 </script>
 
+<div class="confetti-area">
+	<Confetti
+		x={[-5, 5]}
+		y={[0, 0.1]}
+		delay={[500, 2000]}
+		infinite
+		duration={5000}
+		amount={70}
+		fallDistance={`${sectionHeight}px`}
+	/>
+</div>
 <section style:height={`${sectionHeight}px`} class="top-cover"></section>
 
 <style lang="scss">
+	.confetti-area {
+		position: relative;
+		top: -50px;
+		left: 0;
+	}
+
 	section.top-cover {
 		background-image: url('/src/lib/assets/cover-image.jpg');
 		background-repeat: no-repeat;
