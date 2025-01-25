c83ae84731cdc907b600487e6123002a25ed734f
Anthony Park <kunhpark15@gmail.com>
2025-01-25 14:59:44 -0800

    add name, event date and place in top cover

--- src/components/top-cover.svelte
+++ src/components/top-cover.svelte
@@ -30,10 +30,24 @@
 		/>
 	</div>
 
-	<div class="our-names-box">
+	<div class="our-names-kr-box">
 		<span class="our-names kr">박건희</span>
 		<span class="our-names kr">그리고</span>
 		<span class="our-names kr">피아영</span>
+	</div>
+
+	<div class="our-names-en-box">
+		<span class="our-names en-sacramento">Anthony & Emily</span>
+	</div>
+
+	<div class="event-date-and-place-box">
+		{#if localeStore.isKr}
+			<span class="event-date-and-time kr">2025년 6월 7일 (토) 4시</span>
+			<span class="event-place kr">Irvine, CA</span>
+		{:else}
+			<span class="event-date-and-time en">6.7.2025 Sat 4:00 pm</span>
+			<span class="event-place en">Irvine, CA</span>
+		{/if}
 	</div>
 </section>
 
@@ -54,7 +68,7 @@
 		overflow: hidden;
 		pointer-events: none;
 	}
-	.our-names-box {
+	.our-names-kr-box {
 		position: absolute;
 		top: 5%;
 		left: 15%;
@@ -64,6 +78,44 @@
 			display: block;
 			letter-spacing: 1rem;
 			line-height: 3rem;
+		}
+	}
+
+	.our-names-en-box {
+		width: 100%;
+		position: absolute;
+		bottom: 17%;
+		display: flex;
+		justify-content: center;
+
+		span.our-names {
+			display: block;
+			color: $white;
+			font-size: 3.4rem;
+			word-spacing: 0.5rem;
+		}
+	}
+
+	.event-date-and-place-box {
+		width: 100%;
+		position: absolute;
+		bottom: 7%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		span.event-date-and-time,
+		span.event-place {
+			display: block;
+			color: $white;
+
+			&.kr {
+				font-size: 1.4rem;
+			}
+
+			&.en {
+				font-size: 2rem;
+			}
 		}
 	}
 </style>
