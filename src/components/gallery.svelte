07b906abfc9a9bf5b72f73a73c7593aafb647a49
Anthony Park <kunhpark15@gmail.com>
2025-03-23 17:44:29 -0700

    photos gallery

--- src/components/gallery.svelte
+++ src/components/gallery.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
+	import photo1 from '$lib/assets/gallery/1.webp';
+	import photo2 from '$lib/assets/gallery/2.webp';
+	import photo3 from '$lib/assets/gallery/3.webp';
+	import photo4 from '$lib/assets/gallery/4.webp';
+	import photo5 from '$lib/assets/gallery/5.webp';
+	import photo6 from '$lib/assets/gallery/6.webp';
+	import photo7 from '$lib/assets/gallery/7.webp';
+	import photo8 from '$lib/assets/gallery/8.webp';
+	import photo9 from '$lib/assets/gallery/9.webp';
+	import photo10 from '$lib/assets/gallery/10.webp';
 	import PhotoSwipeLightBox from 'photoswipe/lightbox';
 	import PhotoSwipe from 'photoswipe';
 	import 'photoswipe/style.css';
@@ -15,6 +25,59 @@
 
 		lightbox.init();
 	});
+
+	const photos = [
+		{
+			src: photo1,
+			width: 1200,
+			height: 1800
+		},
+		{
+			src: photo2,
+			width: 1200,
+			height: 1800
+		},
+		{
+			src: photo3,
+			width: 1200,
+			height: 1800
+		},
+		{
+			src: photo4,
+			width: 2000,
+			height: 1333
+		},
+		{
+			src: photo5,
+			width: 1200,
+			height: 1800
+		},
+		{
+			src: photo6,
+			width: 2000,
+			height: 1333
+		},
+		{
+			src: photo7,
+			width: 1200,
+			height: 1800
+		},
+		{
+			src: photo8,
+			width: 1200,
+			height: 1800
+		},
+		{
+			src: photo9,
+			width: 1200,
+			height: 1790
+		},
+		{
+			src: photo10,
+			width: 1200,
+			height: 1800
+		}
+	];
 </script>
 
 <section class="gallery">
@@ -23,16 +86,17 @@
 		<p class="sub-title {localeStore.locale}">{$_('gallery.sub_title')}</p>
 	</div>
 	<div id="gallery">
-		<div class="ha"></div>
-		<div class="ha"></div>
-		<div class="ha"></div>
-		<div class="ha"></div>
-		<div class="ha"></div>
-		<div class="ha"></div>
-		<div class="ha"></div>
-		<div class="ha"></div>
-		<div class="ha"></div>
-		<div class="ha"></div>
+		{#each photos as photo}
+			<a
+				href={photo.src}
+				class="photo-slide"
+				data-pswp-width={photo.width}
+				data-pswp-height={photo.height}
+				target="_blank"
+			>
+				<img class="thumbnail" src={photo.src} alt="" />
+			</a>
+		{/each}
 	</div>
 </section>
 
@@ -79,19 +143,21 @@
 		grid-auto-rows: 6.5em;
 	}
 
-	.ha {
+	img.thumbnail {
 		border-radius: 4px;
-		background-color: $font-color-light;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
 	}
 
-	.ha:nth-child(1),
-	.ha:nth-child(2),
-	.ha:nth-child(3),
-	.ha:nth-child(5),
-	.ha:nth-child(7),
-	.ha:nth-child(8),
-	.ha:nth-child(9),
-	.ha:nth-child(10) {
+	.photo-slide:nth-child(1),
+	.photo-slide:nth-child(2),
+	.photo-slide:nth-child(3),
+	.photo-slide:nth-child(5),
+	.photo-slide:nth-child(7),
+	.photo-slide:nth-child(8),
+	.photo-slide:nth-child(9),
+	.photo-slide:nth-child(10) {
 		grid-row: span 2;
 	}
 </style>
