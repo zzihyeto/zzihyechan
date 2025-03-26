e88406edd3f30ac2a4cc58ef63db916e85ca0811
Anthony Park <kunhpark15@gmail.com>
2025-03-25 22:09:33 -0700

    remove event listener

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	let { children } = $props();
 
 	onMount(() => {
-		window.addEventListener('load', () => document.body.classList.add('loaded'));
+		document.body.classList.add('loaded');
 	});
 </script>
 
